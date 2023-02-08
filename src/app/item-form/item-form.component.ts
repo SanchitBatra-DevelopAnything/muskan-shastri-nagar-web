9c3c7fce1ee2ef6d214d39ca7a7953c3dc7ea9a5
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2023-02-08 14:59:01 +0530

    Adding page 2

--- src/app/item-form/item-form.component.ts
+++ src/app/item-form/item-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UtilityService } from '../services/utility.service';
 
 @Component({
@@ -8,17 +9,40 @@ import { UtilityService } from '../services/utility.service';
 })
 export class ItemFormComponent implements OnInit {
 
+  itemForm:FormGroup;
+  imageUrl:string|ArrayBuffer|null;
 
 
   constructor(private utilityService : UtilityService) { }
 
   ngOnInit(): void {
-
+    this.itemForm = new FormGroup({
+      'imgUrl' : new FormControl(''),
+      'weight' : new FormControl(1,[Validators.required]),
+      'quantity' : new FormControl(null , [Validators.required]),
+      'price' : new FormControl(null,[Validators.required]),
+      'totalAmount' : new FormControl(null,[Validators.required]),
+      'balanceAmount' : new FormControl(null,[Validators.required]),
+      'advanceAmount' : new FormControl(null,[Validators.required]),
+      'message' : new FormControl('')
+    });
   }
 
   goBack()
   {
     this.utilityService.formChange.next(1);
   }
+
+  onFileChange(event:any) {
+    const reader = new FileReader();
+    if (event.target.files && event.target.files.length) {
+      const [file] = event.target.files;
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.imageUrl = reader.result;
+      };
+    }
+  }
+  
 
 }
