58bae7a3970151a19b856b637f0c79258a49816f
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2023-02-21 17:59:56 +0530

    Improving inventory ui

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true , 
+  firebaseConfig : {
+    apiKey: "AIzaSyAUqUO6ibegOGNiTVi5KNpiD1vo1-8VjJA",
+  authDomain: "shastri-nagar-shop-app.firebaseapp.com",
+  projectId: "shastri-nagar-shop-app",
+  storageBucket: "shastri-nagar-shop-app.appspot.com",
+  messagingSenderId: "537947242576",
+  appId: "1:537947242576:web:8df08fe99aca961a421155",
+  measurementId: "G-DJLRKMWZP7"
+  }
 };
