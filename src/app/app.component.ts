24403b0dda772bc66907e0e8e4787098eecf9a77
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2023-03-10 00:26:00 +0530

    No header shown on customer order detail page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterEvent } from '@angular/router';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'muskan-shastri-nagar';
+  showHeader:boolean = true;
+
+  constructor(private router:Router) {
+    
+}
+  ngOnInit()
+  {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Check the current route here
+        if (this.router.url.includes("/detail")) {
+          this.showHeader = false; // Set a boolean flag to hide something
+        } else {
+          this.showHeader = true; // Set the flag to show something
+        }
+      }
+    });
+  }
+
 }
